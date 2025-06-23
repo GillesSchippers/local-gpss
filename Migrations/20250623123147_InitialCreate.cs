1a21fa15fe940265aab0130eb8c448f3df68ccbe
GillesSchippers <50411615+GillesSchippers@users.noreply.github.com>
2025-06-23 14:36:45 +0200

    Migrations

---
+++ Migrations/20250623123147_InitialCreate.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace localgpss.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Bundles",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DownloadCode = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    UploadDateTime = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DownloadCount = table.Column<int>(type: "int", nullable: false),
+                    Legal = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    MinGen = table.Column<string>(type: "varchar(32)", maxLength: 32, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    MaxGen = table.Column<string>(type: "varchar(32)", maxLength: 32, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bundles", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Pokemons",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UploadDateTime = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DownloadCode = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DownloadCount = table.Column<int>(type: "int", nullable: false),
+                    Generation = table.Column<string>(type: "varchar(32)", maxLength: 32, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Legal = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Base64 = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Base64Hash = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pokemons", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "BundlePokemons",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PokemonId = table.Column<long>(type: "bigint", nullable: false),
+                    BundleId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BundlePokemons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BundlePokemons_Bundles_BundleId",
+                        column: x => x.BundleId,
+                        principalTable: "Bundles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BundlePokemons_Pokemons_PokemonId",
+                        column: x => x.PokemonId,
+                        principalTable: "Pokemons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BundlePokemons_BundleId",
+                table: "BundlePokemons",
+                column: "BundleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BundlePokemons_PokemonId",
+                table: "BundlePokemons",
+                column: "PokemonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bundles_DownloadCode",
+                table: "Bundles",
+                column: "DownloadCode",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pokemons_Base64Hash",
+                table: "Pokemons",
+                column: "Base64Hash",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pokemons_DownloadCode",
+                table: "Pokemons",
+                column: "DownloadCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BundlePokemons");
+
+            migrationBuilder.DropTable(
+                name: "Bundles");
+
+            migrationBuilder.DropTable(
+                name: "Pokemons");
+        }
+    }
+}
